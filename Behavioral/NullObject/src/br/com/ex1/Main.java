52119dc63158285e603aa473fe135691028cf198
GuiRodriguero <50684900+GuiRodriguero@users.noreply.github.com>
2023-09-06 14:28:22 -0300

    Delete Behavioral/NullObject directory

--- Behavioral/NullObject/src/br/com/ex1/Main.java
+++
@@ -1,8 +0,0 @@
-package br.com.ex1;
-
-public class Main {
-    public static void main(String[] args) {
-        ComplexService service = new ComplexService("Simple report 2",new NullStorageService());
-        service.generateReport();
-    }
-}
