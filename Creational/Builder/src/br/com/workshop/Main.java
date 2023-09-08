9eb9cec8cdff2a98b8fbaf9870f2c1dcefb018c7
GuiRodriguero <50684900+GuiRodriguero@users.noreply.github.com>
2023-09-08 11:45:26 -0300

    Exercício Builder

--- Creational/Builder/src/br/com/workshop/Main.java
+++ Creational/Builder/src/br/com/workshop/Main.java
@@ -8,13 +8,16 @@ public class Main {
         *
         * Criar Builder -> Criar Concrete Builder -> Criar Director
         *
-        * 1 - Criar interface TesteBuilder (Builder). Este Builder deve ter 1 método para cada parâmetro (name, material, valorReferencia) do nosso "Product" (Retornando o Builder)
-        * e 1 método para juntar tudo (Retornando o objeto alvo da criação)
+        * 1 - Criar interface TesteBuilder (Builder). Este Builder deve ter 1 método para cada parâmetro (nome, material, valorReferencia) do nosso "Product" (Teste) (Retornando o Builder)
+        * e 1 método para juntar tudo (Retornando o objeto que estamos querendo criar) Ex.: Teste build();
         *
-        * 2 - Criar implementação da Interface (Concrete Builder).
-        * devemos também declarar os atributos do nosso "Product". Cada método deve retornar this.
+        * 2 - Criar implementação da Interface TesteBuilder (Concrete Builder).
+        * Declarar os atributos do nosso "Product".
+        * Implementar os métodos da interface: Cada método deve atribuir o valor obtido como parâmetro (this.nome = nome) e retornar this.
         *
-        * 3 - No Main, criar um método estático que recebe o Builder e retorna o objeto criado (Director)
+        * 3 - No Main, criar um método estático que instancia o Builder.
+        *       Agora podemos tirar proveito do encadeamento de métodos que o "return this;" nos oferece. Com o builder em mãos, podemos
+        *       fazer: builder.metodo1("xxx").metodo2(123).build();
         */
     }
 }
